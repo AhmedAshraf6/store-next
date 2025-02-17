353540a1c3a806b4fb44484b82c183b8d25520c4
AhmedAshraf6 <ahmedp76b@gmail.com>
2025-02-17 20:44:44 +0200

    fix isseus 5

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,14 +1,14 @@
 import ProductsContainer from '@/components/products/ProductsContainer';
 
-interface ProductsPageProps {
-  searchParams?: Record<string, string | string[] | undefined>;
-}
-
-export default async function ProductsPage({
+async function ProductsPage({
   searchParams,
-}: ProductsPageProps) {
-  const layout = (await searchParams)?.layout?.toString() || 'grid';
-  const search = (await searchParams)?.search?.toString() || '';
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const layout = (await searchParams).layout?.toString() || 'grid';
+  const search = (await searchParams).search?.toString() || '';
 
   return <ProductsContainer layout={layout} search={search} />;
 }
+
+export default ProductsPage;
