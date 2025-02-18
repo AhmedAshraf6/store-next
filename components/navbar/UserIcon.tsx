f05c730b6a808aec4528dc8266c256b48bb1d801
AhmedAshraf6 <ahmedp76b@gmail.com>
2025-02-18 00:50:29 +0000

    auth

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,4 +1,14 @@
-function UserIcon() {
-  return <div>UserIcon</div>;
+import { auth, currentUser } from '@clerk/nextjs/server';
+import { LuUser } from 'react-icons/lu';
+
+async function UserIcon() {
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+  if (profileImage) {
+    return (
+      <img className='w-6 h-6 rounded-full object-cover' src={profileImage} />
+    );
+  }
+  return <LuUser className='w-6 h-6 rounded-full text-white' />;
 }
 export default UserIcon;
